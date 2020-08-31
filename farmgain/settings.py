6fb8b2849927cdca036a2e3225a023273776380c
maximugisha <maximugisha@gmail.com>
2020-08-31 12:33:26 +0300

    added procfile and edited settings fro heroku deploymenr

--- farmgain/settings.py
+++ farmgain/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 """
 Django settings for farmgain project.
 
@@ -122,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# Activate Django-Heroku.
+django_heroku.settings(locals())
